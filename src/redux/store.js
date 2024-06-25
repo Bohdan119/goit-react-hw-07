c0a26ec31323edb8a52374cc8e3c1fe6bfa1e416
Bohdan Matsko <bm01271970@gmail.com>
2024-06-25 21:22:56 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./rootReducer";
 
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+    getDefaultMiddleware(),
 });
 
